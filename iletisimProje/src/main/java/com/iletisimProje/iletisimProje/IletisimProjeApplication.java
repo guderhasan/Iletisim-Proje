635d70b35dd7e3046d4fc7d7843f7efe0f311aea
guderhasan <68234046+guderhasan@users.noreply.github.com>
2025-06-19 15:28:49 +0300

    Add files via upload

---
+++ iletisimProje/src/main/java/com/iletisimProje/iletisimProje/IletisimProjeApplication.java
@@ -0,0 +1,13 @@
+package com.iletisimProje.iletisimProje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IletisimProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IletisimProjeApplication.class, args);
+	}
+
+}
---
+++ iletisimProje/src/test/java/com/iletisimProje/iletisimProje/IletisimProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iletisimProje.iletisimProje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IletisimProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
